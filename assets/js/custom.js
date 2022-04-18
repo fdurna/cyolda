ab58a38b51daceef324174af0a58ede280f2b9f7
fatih.durna <fdfatihdurna@gmail.com>
2022-04-18 20:11:23 +0300

    dev

---
+++ assets/js/custom.js
@@ -0,0 +1,19 @@
+// Ready Functions
+$(document).ready(function () {
+  demo();
+});
+
+// Preloader Start
+window.addEventListener('load', function () {
+  this.setTimeout(function () {
+    $('#preloader').delay(2000).fadeOut(500);
+  }, 500);
+});
+// Preloader End
+
+// Accordion FAQ
+var demo = function () {
+  console.log("selam")
+};
+// Accordion FAQ End
+
