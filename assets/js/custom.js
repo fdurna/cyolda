2c83c4d42ecb62bcc1a2df6c659adcbbbe8fb0db
fatih.durna <fdfatihdurna@gmail.com>
2022-04-24 02:04:09 +0300

    dev

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,19 +1,67 @@
 // Ready Functions
 $(document).ready(function () {
-  demo();
+  moreProduct();
+  if($(".counter").length) {
+    counterDown();
+  }
 });
 
 // Preloader Start
-window.addEventListener('load', function () {
+window.addEventListener("load", function () {
   this.setTimeout(function () {
-    $('#preloader').delay(2000).fadeOut(500);
+    $("#preloader").delay(2000).fadeOut(500);
   }, 500);
 });
 // Preloader End
 
-// Accordion FAQ
-var demo = function () {
-  console.log("selam")
+// moreProduct FAQ
+var moreProduct = function () {
+  $("#moreProduct").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $("#titlesubtitle").offset().top,
+      },
+      1000
+    );
+  });
 };
-// Accordion FAQ End
+// moreProduct FAQ End
 
+// counterDown FAQ
+var counterDown = function () {
+  const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
+
+  let today = new Date(),
+    dd = String(today.getDate()).padStart(2, "0"),
+    mm = String(today.getMonth() + 1).padStart(2, "0"),
+    yyyy = today.getFullYear(),
+    nextYear = yyyy + 1,
+    dayMonth = "09/30/",
+    birthday = dayMonth + yyyy;
+
+  today = mm + "/" + dd + "/" + yyyy;
+  if (today > birthday) {
+    birthday = dayMonth + nextYear;
+  }
+
+  const countDown = new Date(birthday).getTime(),
+    x = setInterval(function () {
+      const now = new Date().getTime(),
+        distance = countDown - now;
+        (document.getElementById("days").innerText = Math.floor(distance / day)),
+
+        (document.getElementById("hours").innerText = Math.floor(
+          (distance % day) / hour
+        )),
+        (document.getElementById("minutes").innerText = Math.floor(
+          (distance % hour) / minute
+        )),
+        (document.getElementById("seconds").innerText = Math.floor(
+          (distance % minute) / second
+        ));
+    }, 0);
+};
+// counterDown FAQ End
